14c38e60760fb5b928fa781ab1baad4d39109669
Umar Hemmad <hemmad777@gmail.com>
2025-12-11 16:52:00 +0530

    Created category section and finish alsmost style

--- src/components/categorySection/category.jsx
+++ src/components/categorySection/category.jsx
@@ -18,14 +18,14 @@ function Category(){
 
     return(
         <>
-        <div className='w-full'>
-            <h2 className=''>Categories</h2>
+        <div className='w-full pt-12 pb-12 bg-light'>
+            <h2 className='text-5xl font-bold text-black'>CATEGORIES</h2>
 
-            <div className='cat-list grid grid-cols-3 round-full'>
+            <div className='cat-list grid grid-cols-2 sm:grid-cols-3 place-items-center pt-6 gap-5'>
                 {categoryList?.map((item)=>(
-                    <div className='cat-card' key={item._id}>
-                        <img className='w-full ' src={`http://localhost:3005/${item.image}`} alt={item.name} />
-                        <h4>{item.name}</h4>
+                    <div className='cat-card border border-gray-400 rounded-lg shadow-2xl shadow-black' key={item._id}>
+                        <img className='w-[15rem] h-[15rem] rounded-full' src={`http://localhost:3005/${item.image}`} alt={item.name} />
+                        <h4 className='text-3xl font-bold text-black'>{item.name}</h4>
                     </div>
                 ))}
             </div>
