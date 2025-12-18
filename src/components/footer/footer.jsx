4eaf7c55ed56e321b4119b23f0fb795560d86c91
Umar Hemmad <hemmad777@gmail.com>
2025-12-18 19:59:00 +0530

    Added contact icon in navbar

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 function Footer() {
     return(
         <>
-        <section className="Footer w-full bg-black/90 h-32 text-white grid">
+        <section className="Footer w-full bg-black/90 h-32 text-white grid items-center">
             <p><span>instagram</span> <span>@instagram</span></p>
             <p><span>twitter</span> <span>@twitter</span></p>
             <p><span>whatsapp</span> <span>+91 1234567890</span></p>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 import "./navbar.css";
 
 function Navbar(){
@@ -8,6 +9,9 @@ function Navbar(){
                 <Link to="/">Home</Link>
                 <Link to="/Collections">products</Link>
                 <Link to="/About">About</Link>
+                <Link to="/me">   
+                <FaUserCircle className="text-2xl text-white hover:text-black cursor-pointer"></FaUserCircle>
+                </Link>
             </div>
         </>
     )
