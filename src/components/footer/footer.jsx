f645bc3150682ce8dc748bf5a1d03cf6349e1bd9
Umar Hemmad <hemmad777@gmail.com>
2025-12-18 19:40:38 +0530

    created footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,8 +3,10 @@ import "./footer.css"
 function Footer() {
     return(
         <>
-        <section className="Footer w-full bg-black/90 h-32">
-            
+        <section className="Footer w-full bg-black/90 h-32 text-white grid">
+            <p><span>instagram</span> <span>@instagram</span></p>
+            <p><span>twitter</span> <span>@twitter</span></p>
+            <p><span>whatsapp</span> <span>+91 1234567890</span></p>
         </section>
         </>
     )
